9acbad57de50336380f2326d485da37e96275a67
Igor <igorpolvora@gmail.com>
2025-08-07 09:30:41 -0300

    Cria modelos Post e Comment

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,6 @@
+# posts/admin.py
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.content[:30]}..."
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário de {self.user.username} em {self.post.id}"
